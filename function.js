d7517dfd88613084936657a348fd1933177e53ed
hecktech <32950689+hecktech@users.noreply.github.com>
2024-09-22 12:45:32 +0530

    Update function.js

--- function.js
+++ function.js
@@ -17,10 +17,10 @@ document.querySelector(".dice2>img").setAttribute("src",player2);
 
 
 if(player1>player2){
-    document.querySelector("h1").innerHTML=" 🎉 "+player1name;
+    document.querySelector("h1").innerHTML=player1name+" Win;
 }
 else if(player2>player1){
-    document.querySelector("h1").innerHTML= player2name +" 🎉";
+    document.querySelector("h1").innerHTML= player2name +" Win";
 }
 else{
     document.querySelector("h1").innerHTML=" Draw! ";
